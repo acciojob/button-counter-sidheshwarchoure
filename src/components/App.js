6e0ca38f78dcd2562adf5258894a977e35b1f501
Sidheshwar Choure <sidheshwarchoure@gmail.com>
2023-07-14 19:33:53 +0530

    newchange

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
 import ButtonCounter from './ButtonCounter';
 
 function App() {
+
+  let [count,setCount] = useState(0);
+
   return (
     <div>
-      <ButtonCounter />
+      <p>Button clicked {count} times</p>
+      <button onClick={()=>setCount(count+1)}>Click me</button>
     </div>
   );
 }
